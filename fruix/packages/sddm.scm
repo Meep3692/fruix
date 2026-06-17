521f1bc1d6bb0d5c55ccf3c07a32bff1bd736840
Aida Smith <meep3692@gmail.com>
2026-06-17 01:35:56 -0400

    Add some commonality themes

--- fruix/packages/sddm.scm
+++ fruix/packages/sddm.scm
@@ -26,3 +26,47 @@
    (synopsis "A sddm theme that looks like xdm")
    (description "A sddm theme that looks like xdm")
    (license license:cc-by-sa3.0)))
+
+(define-public sddm-commonality-theme
+  (package
+   (name "sddm-commonality-theme")
+   (version "92b859b8dd84bac7f1a8c72b8428fee98405f06a")
+   (source (origin
+            (method git-fetch)
+            (uri
+              (git-reference
+              (url "https://www.opencode.net/phob1an/commonality.git")
+              (commit version)))
+            (sha256
+              (base32 "15ckhbdd4zsma4xn4rsdndvi5j6aaqm8w12l4cdfyi2v9s1vhwv0"))))
+   (build-system copy-build-system)
+   (arguments (list
+    #:install-plan
+      #~'(("./sddm/themes" "share/sddm/themes"))))
+   (home-page "https://github.com/zebreus/sddm-xdm-theme")
+   (synopsis "Remembering CDE")
+   (description "An sddm theme made to resemble xdm on a CDE system.")
+   (license license:gpl3)))
+
+(define-public sddm-commonality-sol-theme
+  (package
+   (name "sddm-commonality-sol-theme")
+   (version "92b859b8dd84bac7f1a8c72b8428fee98405f06a")
+   (source (origin
+            (method git-fetch)
+            (uri
+              (git-reference
+              (url "https://www.opencode.net/phob1an/commonality.git")
+              (commit version)))
+            (sha256
+              (base32 "15ckhbdd4zsma4xn4rsdndvi5j6aaqm8w12l4cdfyi2v9s1vhwv0"))))
+   (build-system copy-build-system)
+   (arguments (list
+    #:install-plan
+      #~'(("./SOL/sddm/themes" "share/sddm/themes"))))
+   (home-page "https://github.com/zebreus/sddm-xdm-theme")
+   (synopsis "Remembering CDE")
+   (description "An sddm theme made to resemble xdm on a CDE system.")
+   (license license:gpl3)))
+
+sddm-commonality-sol-theme
